31b84b81d3c7e5eec19d15d47e4ac755c0fc7727
ouroobatariq <62900041+ouroobatariq@users.noreply.github.com>
2020-05-11 02:26:39 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Myform from './component/Myform';
+
+
+function App() {
+  return (
+    <div className="App">
+   <Myform/>
+    </div>
+  );
+}
+
+export default App;
